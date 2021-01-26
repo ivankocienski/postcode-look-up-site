d8130c0cf96377f7f42b2638e482d6478bc80b57
Ivan Kocienski <ivan.kocienski@gmail.com>
2021-01-26 18:32:49 +0000

    Looks better

--- app/db.py
+++ app/db.py
@@ -9,8 +9,9 @@ class Store:
         self.id = values[0]
         self.name = values[1]
         self.postcode = values[2]
-        self.longitude = values[3]
-        self.latitude = values[4]
+        self.latitude = values[3]
+        self.longitude = values[4]
+        
 
 def stores_count():
     conn = db_engine.connect()
@@ -19,23 +20,23 @@ def stores_count():
 
 def stores_find_all():
     conn = db_engine.connect()
-    sql = 'select id, name, postcode, longitude, latitude from stores order by name'
+    sql = 'select id, name, postcode, latitude, longitude from stores order by name'
     stores = [ Store(row) for row in conn.execute(sql)]
     return stores
 
-def stores_insert(name, postcode):
-    for store in data:
-    name = store['name']
-    postcode = store['postcode']
-
-    cursor.execute('insert into stores (name, postcode) values (?, ?)', (name, postcode))
-    # print("store=", store)
-
-def stores_update_position(id, latitude, longitude):
-    # def update_store(db, id, new_latitude, new_longitude):
-    sql = 'update stores set latitude=?, longitude=? where id=?'
-    args = (new_latitude, new_longitude, id)
-
-    cursor = db.cursor()
-    cursor.execute(sql, args)
-    db.commit()
+#def stores_insert(name, postcode):
+#    for store in data:
+#    name = store['name']
+#    postcode = store['postcode']
+#
+#    cursor.execute('insert into stores (name, postcode) values (?, ?)', (name, postcode))
+#    # print("store=", store)
+#
+#def stores_update_position(id, latitude, longitude):
+#    # def update_store(db, id, new_latitude, new_longitude):
+#    sql = 'update stores set latitude=?, longitude=? where id=?'
+#    args = (new_latitude, new_longitude, id)
+#
+#    cursor = db.cursor()
+#    cursor.execute(sql, args)
+#    db.commit()
--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ import app.config as config
 
 app = Flask(
     config.name(), 
-    static_url_path=config.work_dir,
+    static_url_path='/',
     static_folder=config.work_dir + '/public',
     template_folder=config.work_dir + '/app/views'
 )
